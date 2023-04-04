aca69596d936dc7d0ad87cb03c65b2a67899efe2
HugoSanche <hbmclaren@gmail.com>
2023-04-03 22:54:35 -0600

    Continue working with lambda expression with interfaces Function & Supplier

--- Lambda Expression/src/OtherExamble2.java
+++ Lambda Expression/src/OtherExamble2.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public class OtherExamble2 {
     public static void main(String[] args) {
@@ -15,9 +17,22 @@ public class OtherExamble2 {
         var firstPoint=coordenadas.get(0);
         processPoint(firstPoint[0],firstPoint[1],p1);
         coordenadas.forEach(x->processPoint(x[0],x[1],p1));
-
+        String[] na= {"Hugo","Pedro","Paco","Caralina","Veronica","Lucia"};
+        String[] ramdomList=ramdomStringNames(20,na,
+                //check no parameters in this lambda expression
+                //because interface supplier have method get() who don't have any parameters
+                ()->new Random().nextInt(0,na.length)
+                );
+        System.out.println(Arrays.toString(ramdomList));
     }
     public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer){
         consumer.accept(t1, t2);
+    }
+    public static String[] ramdomStringNames(int count, String[] names, Supplier<Integer> s){
+        String[] values=new String[count];
+        for (int i=0; i<count; i++){
+            values[i]=names[s.get()];//execute method get() of Supplier interface
+        }
+        return values;
     }
 }
--- Lambda Expression/src/OtherExample.java
+++ Lambda Expression/src/OtherExample.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OtherExample {
@@ -8,14 +9,14 @@ public class OtherExample {
 //            System.out.println(n);
 //        }
         String año=" 2000";
-        //String myString="Error because I used myString in lambda expresion in forEach";
+        //String myString="Error because I used myString in lambda expression in forEach";
         names.forEach(myString -> { //here i used variable myString
 
             String escuela="TESE ";
             System.out.println(escuela+myString+año);
 
         });
-        //año="2005"; you can't modify the variable año
+        //año="2005"; you can't modify the variable año because you used in lambda expression
         //System.out.println(escuela); error does not recognize escuela
 
         //Predicate Lambda expression
@@ -28,6 +29,35 @@ public class OtherExample {
         System.out.println("------");
         names.removeIf(x->x.startsWith("Pa"));
         names.forEach(x->System.out.println(x));
+
+        System.out.println("Examples lambda expression for function");
+        //Examples lambda expression for function
+        System.out.println("------------");
+        names.replaceAll(x->x.charAt(0)+" "+x.toUpperCase());
+        System.out.println(names);
+
+        String[] emptyString=new String[10];
+        System.out.println(Arrays.toString(emptyString));
+
+
+        Arrays.fill(emptyString,"");
+        System.out.println(Arrays.toString(emptyString));
+
+        Arrays.setAll(emptyString,(i)->
+                ""+(i+1)+". "+
+                        switch (i){
+                            case 0->"one";
+                            case 1->"two";
+                            case 2->"three";
+                            case 3->"four";
+                            case 4->"five";
+                            default -> "";
+                }
+        );
+        System.out.println(Arrays.toString(emptyString));
+
+        System.out.println("Examples lambda expression for the supplier interface");
+        //Examples lambda expression for the supplier interface
 
     }
 
